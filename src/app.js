c6f41cadeb989532aa51ea924f4496a9d2f2b862
Mehtap Celik <mehtapcelik284@gmail.com>
2021-12-01 16:13:02 +0300

    firebase config has been seperated

--- src/app.js
+++ src/app.js
@@ -2,21 +2,12 @@ import './app.css';
 import { Exchange } from './pages';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from './utils/settings';
 
 const App = () => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyALXSEaiLbFxp7optCZUBRsK_-_aCJNuVI",
-    authDomain: "currency-exchange-prototype.firebaseapp.com",
-    projectId: "currency-exchange-prototype",
-    storageBucket: "currency-exchange-prototype.appspot.com",
-    messagingSenderId: "18444262527",
-    appId: "1:18444262527:web:e102f1e544984d7eb0334a",
-    measurementId: "G-4MTX0ENYJN"
-  };
-
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  getAnalytics(app);
 
   return (
     <Exchange />
---
+++ src/utils/settings.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALXSEaiLbFxp7optCZUBRsK_-_aCJNuVI",
+    authDomain: "currency-exchange-prototype.firebaseapp.com",
+    projectId: "currency-exchange-prototype",
+    storageBucket: "currency-exchange-prototype.appspot.com",
+    messagingSenderId: "18444262527",
+    appId: "1:18444262527:web:e102f1e544984d7eb0334a",
+    measurementId: "G-4MTX0ENYJN"
+};
+
+export default firebaseConfig;
